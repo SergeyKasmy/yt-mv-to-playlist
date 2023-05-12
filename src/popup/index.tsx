7291214ea0164185d4ef4652e0ea61b9ba410eb3
Sergey Kasmy <SleeplessSloth79@gmail.com>
2023-05-12 05:28:07 +0200

    Use React StrictMode

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,6 +1,11 @@
 import Popup from "./popup.tsx";
 import { throwExpr } from "../utils.ts";
 import { createRoot } from "react-dom/client";
+import React from "react";
 
 const root = document.getElementById("root") ?? throwExpr("Root div not found");
-createRoot(root).render(<Popup />);
+createRoot(root).render(
+	<React.StrictMode>
+		<Popup />
+	</React.StrictMode>
+);
