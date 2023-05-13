836edbf587345b3a220b4c02ab22bad5ac2474bf
Sergey Kasmy <SleeplessSloth79@gmail.com>
2023-05-13 05:24:38 +0200

    Detect loading spinner when scrolling to end

--- src/content/scroll_to_end.ts
+++ src/content/scroll_to_end.ts
@@ -2,27 +2,30 @@ import { sleep } from "../utils.ts";
 
 export default class ScrollToEnd {
 	running = true;
-	private retryCount = 0;
 
 	async scrollToEnd() {
 		// eslint-disable-next-line no-constant-condition
 		while (true) {
-			if (!this.running || this.retryCount > 60) {
+			if (!this.running) {
 				console.log("Stopping scrolling to end");
 				return;
 			}
 			// still left to scroll
 			if (
-				document.documentElement.scrollTop <
+				document.documentElement.scrollTop ==
 				document.documentElement.scrollHeight -
 					document.documentElement.clientHeight
 			) {
-				document.documentElement.scrollTop =
-					document.documentElement.scrollHeight;
-			} else {
-				this.retryCount += 1;
+				// if spinner not hidden, i.e. not loading
+				if (document.getElementById("spinner")?.ariaHidden !== "true") {
+					console.log("Scrolled to end");
+					this.running = false;
+					return;
+				}
 			}
 
+			document.documentElement.scrollTop =
+				document.documentElement.scrollHeight;
 			await sleep(1);
 		}
 	}
